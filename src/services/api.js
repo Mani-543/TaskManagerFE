6e1daf8f19da7b237a661c7bec1c7643e44caab4
Manikanta <manikanta543@gmail.com>
2026-04-24 16:35:47 +0530

    update api service

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://taskmanagerbackend-j136.onrender.com//api",
+  baseURL: "https://taskmanagerbackend-j136.onrender.com/api",
 });
 
 // Automatically attach token
